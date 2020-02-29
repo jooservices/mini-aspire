fd76d9382453d594dae8b861f6eb201cf14880b5
Viet Vu <jooservices@gmail.com>
2020-02-29 22:31:46 +0700

    ^ Prevent repayment amount lower than 0.0

--- src/Controller/LoansController.php
+++ src/Controller/LoansController.php
@@ -170,6 +170,10 @@ class LoansController extends BaseController
         $fullName = $request->get('full_name');
         $amount   = (double)$request->get('amount');
 
+        if ($amount <= 0.0) {
+            return $this->respondBad('Amount is lower than 0.0');
+        }
+
         if (!$fullName || !$amount) {
             return $this->respondBad();
         }
